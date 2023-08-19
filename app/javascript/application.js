96ae020016eb7e0b58bd353b4542e9a037dcdd7c
Martin Sugasti <martinsugasti@gmail.com>
2023-08-19 16:58:22 +0200

    Changes that make bootstrap tooltips work

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,3 +2,10 @@
 import '@hotwired/turbo-rails';
 import './controllers';
 import * as bootstrap from 'bootstrap';
+
+// Added for making bootstrap tooltips work
+document.addEventListener('turbo:load', () => {
+  const tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
+
+  tooltipTriggerList.map((tooltipTriggerEl) => new bootstrap.Tooltip(tooltipTriggerEl));
+});
