f34481cff3a4ec71d69ee1bafd654850a937df20
Martin Sugasti <martinsugasti@gmail.com>
2023-09-17 18:38:23 +0200

    Make sidebar show selected menu

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,6 +3,10 @@ module ApplicationHelper
     user_signed_in? && controller_name != 'pages'
   end
 
+  def current_sidebar_option?(*paths)
+    paths.any? { |path| current_page?(path) }
+  end
+
   def omniauth_authorize_path_for(resource, provider)
     public_send("#{resource.class.name.downcase}_#{provider}_omniauth_authorize_path")
   end
